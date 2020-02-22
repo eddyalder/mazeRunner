8ee795e1860eb4d5b87ad6cbd5338374e451a89e
eddyalder <59810975+eddyalder@users.noreply.github.com>
2020-02-22 11:52:00 -0700

    Added movement and boundaries at the endge

--- mainEdward.py
+++ mainEdward.py
@@ -3,13 +3,18 @@ pygame.init()
 
 screenWidth = 700
 screenHeight = 500
+x = 100
+y = 100
 
 win = pygame.display.set_mode((screenWidth,screenHeight))
 white = (255,255,255)
+red = (255,0,0)
 black = (0,0,0)
 win.fill(white)
-pygame.display.set_caption("First Game")
-pygame.draw.rect(win, black, (200,150,100,50))
+radius = 12
+pygame.display.set_caption("The Maze")
+
+clock = pygame.time.Clock()
 
 run = True
 
@@ -17,4 +22,19 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    pygame.display.update()    
+    
+    pressed = pygame.key.get_pressed()
+    if (pressed[pygame.K_UP] or pressed[pygame.K_w]) and y > radius: 
+        y -= 5
+    if (pressed[pygame.K_DOWN] or pressed[pygame.K_s]) and y < screenHeight - radius: 
+        y += 5
+    if (pressed[pygame.K_LEFT] or pressed[pygame.K_a]) and x > radius: 
+        x -= 5
+    if (pressed[pygame.K_RIGHT] or pressed[pygame.K_d]) and x < screenWidth - radius: 
+        x += 5
+    #print("x", x, "y", y)
+    win.fill(white)
+    pygame.draw.circle(win, red, (x,y), radius)
+
+    pygame.display.flip()
+    clock.tick(144)
