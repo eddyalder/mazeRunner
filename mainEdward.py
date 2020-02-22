10438c4c04db07dc90ddc0164ea1bd172553397d
eddyalder <59810975+eddyalder@users.noreply.github.com>
2020-02-22 13:34:23 -0700

    Cleaned up everything in mainEdward

--- mainEdward.py
+++ mainEdward.py
@@ -1,40 +1,54 @@
 import pygame
 pygame.init()
 
-screenWidth = 700
-screenHeight = 500
-x = 100
-y = 100
-
-win = pygame.display.set_mode((screenWidth,screenHeight))
+#colors
 white = (255,255,255)
 red = (255,0,0)
 black = (0,0,0)
+
+#screen setup
+screenWidth = 700
+screenHeight = 500
+
+win = pygame.display.set_mode((screenWidth,screenHeight))
 win.fill(white)
-radius = 12
 pygame.display.set_caption("The Maze")
 
-clock = pygame.time.Clock()
+class Player(object):
+    def __init__(self,x,y,color,radius):
+        self.x = x
+        self.y = x
+        self.color = red
+        self.radius = radius
+        self.velocity = 5
 
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x,self.y), self.radius)
+    
+
+def redrawGameWindow():
+    win.fill(white)
+    circle.draw(win)
+    pygame.display.update()
+
+clock = pygame.time.Clock()
+circle = Player(100,100,red,12)
 run = True
 
 while run:
+    clock.tick(144)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     
     pressed = pygame.key.get_pressed()
-    if (pressed[pygame.K_UP] or pressed[pygame.K_w]) and y > radius: 
-        y -= 5
-    if (pressed[pygame.K_DOWN] or pressed[pygame.K_s]) and y < screenHeight - radius: 
-        y += 5
-    if (pressed[pygame.K_LEFT] or pressed[pygame.K_a]) and x > radius: 
-        x -= 5
-    if (pressed[pygame.K_RIGHT] or pressed[pygame.K_d]) and x < screenWidth - radius: 
-        x += 5
+    if (pressed[pygame.K_UP] or pressed[pygame.K_w]) and circle.y > circle.radius: 
+        circle.y -= circle.velocity
+    if (pressed[pygame.K_DOWN] or pressed[pygame.K_s]) and circle.y < screenHeight - circle.radius: 
+        circle.y += circle.velocity
+    if (pressed[pygame.K_LEFT] or pressed[pygame.K_a]) and circle.x > circle.radius: 
+        circle.x -= circle.velocity
+    if (pressed[pygame.K_RIGHT] or pressed[pygame.K_d]) and circle.x < screenWidth - circle.radius: 
+        circle.x += circle.velocity
     #print("x", x, "y", y)
-    win.fill(white)
-    pygame.draw.circle(win, red, (x,y), radius)
-
-    pygame.display.flip()
-    clock.tick(144)
+    redrawGameWindow()
